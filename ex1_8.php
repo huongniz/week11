366f7e56c8749969839369f19fd2dc6c0f128ff8
huongniz <22070956@vnu.edu.vn>
2025-11-16 22:58:56 +0700

    Create ex1_8.php

---
+++ ex1_8.php
@@ -0,0 +1,8 @@
+<?php
+$cookieName = "visited";
+if (isset($_COOKIE[$cookieName])) {
+    echo "Welcome back! You have visited before.";
+} else {
+    echo "Welcome! This is your first visit.";
+}
+?>
